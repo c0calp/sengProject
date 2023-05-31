259c80d12f0523fffc668d048882e00ed9da1a82
Slim Shady <bakgokalp@gmail.com>
2023-05-31 03:15:28 +0300

    first commit

---
+++ static-webiste-with-node/server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+const nodemailer = require('nodemailer');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname+ '/public/order.html'));
+});
+
+app.listen(port, ()=> {
+    console.log("Server is running on "+port)
+})
+app.post("/send_email", function(req, response){
+    var to = req.body.to;
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'duyicorestaurant@gmail.com',
+            pass: 'eiwgicjooapbzgis'
+            
+        }
+    });
+
+    var mailOptions = {
+        from: "duyicorestaurant@gmail.com",
+        to: to,
+        subject: "Your Order",
+        text: "Your order has taken place. Thanks for choosing us!"
+    }
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if(error){
+            console.log(error);
+        } else {
+            console.log("Email send: " + info.response)
+        }
+        response.redirect("/")
+    });
+});
+
+//initialize web server
